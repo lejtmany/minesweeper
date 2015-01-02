3aee1adfe7d96276d98320ff45507bf9e62a03c7
lejtmany <lejtmany@b6a8b88c-56e7-d033-4fd6-d9ff7ddcd776>
2015-01-02 03:00:33 +0000

    flag and bomb images in gui work

--- Minesweeper/MinesweeperController.cs
+++ Minesweeper/MinesweeperController.cs
@@ -8,11 +8,11 @@ using System.Windows.Forms;
 
 namespace Minesweeper
 {
-   class MinesweeperController
-    {        
+    class MinesweeperController
+    {
         internal Board board;
         private Minesweeper gui;
-        
+
         public MinesweeperController(Minesweeper gui, Board board)
         {
             this.board = board;
@@ -22,18 +22,19 @@ namespace Minesweeper
 
         private void SetUpButtonHandlers()
         {
-            for(int i = 0; i < gui.buttonArray.GetLength(0); i++)
+            for (int i = 0; i < gui.buttonArray.GetLength(0); i++)
                 for (int j = 0; j < gui.buttonArray.GetLength(0); j++)
                 {
                     gui.buttonArray[i, j].Click += button_Click;
-                    gui.buttonArray[i,j].MouseUp += MinesweeperController_MouseUp;
+                    gui.buttonArray[i, j].MouseUp += MinesweeperController_MouseUp;
                 }
         }
 
         private void MinesweeperController_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
         {
-            if(e.Button == MouseButtons.Right){
-                MinesweeperButton button = (MinesweeperButton) sender;
+            if (e.Button == MouseButtons.Right)
+            {
+                MinesweeperButton button = (MinesweeperButton)sender;
                 Point buttonLocation = button.Coordinates;
                 BoardSquare modelSquare = board.GetSquare(buttonLocation.X, buttonLocation.Y);
                 modelSquare.IsFlag = !modelSquare.IsFlag;
@@ -43,7 +44,7 @@ namespace Minesweeper
 
         private void button_Click(object sender, EventArgs e)
         {
-            MinesweeperButton button = (MinesweeperButton) sender;
+            MinesweeperButton button = (MinesweeperButton)sender;
             Point buttonLocation = button.Coordinates;
             BoardSquare modelSquare = board.GetSquare(buttonLocation.X, buttonLocation.Y);
             board.ClickSquare(modelSquare);
@@ -53,30 +54,31 @@ namespace Minesweeper
         private void updateView(BoardSquare selectedButton)
         {
             BoardSquare square;
-            if(selectedButton.value == BoardSquare.BOMB)
-                gui.buttonArray[selectedButton.X, selectedButton.Y].BackColor = Color.Red;
-            for(int i = 0; i < board.Width; i++)
-               for (int j = 0; j < board.Height; j++)
-               {
-                   square = board.GetSquare(i, j);
-                   if (board.gameOver)
-                   {
-                       if (square.value == BoardSquare.BOMB)
-                       {
-                           gui.buttonArray[i, j].Image = new Bitmap(new Bitmap(@"C:\Users\Miriam\Google Drive\Visual Studio\minesweeper-mco368\Minesweeper\bomb-icon.png"), gui.buttonArray[i, j].Width - 5, gui.buttonArray[i, j].Height - 5);                           
-                       }
 
-                   }
-                   if(square.IsFlag)
-                       gui.buttonArray[i, j].Image = new Bitmap(new Bitmap(@"C:\Users\Miriam\Google Drive\Visual Studio\minesweeper-mco368\Minesweeper\flag-icon.png"), gui.buttonArray[i, j].Width - 5, gui.buttonArray[i, j].Height - 5);
-                   if (!square.IsFlag)
-                       gui.buttonArray[i, j].Image = null;
-                   if (square.isOpen)
-                   {
-                       gui.buttonArray[i, j].Text = square.value.ToString();
-                       gui.buttonArray[i, j].Enabled = false;
-                   }
-               } 
+            if (selectedButton.value == BoardSquare.BOMB)
+                gui.buttonArray[selectedButton.X, selectedButton.Y].BackColor = Color.Red;
+
+            for (int i = 0; i < board.Width; i++)
+                for (int j = 0; j < board.Height; j++)
+                {
+                    gui.buttonArray[i, j].Image = null;
+                    square = board.GetSquare(i, j);
+                    if (board.gameOver)
+                    {
+                        if (square.value == BoardSquare.BOMB)
+                        {
+                            gui.buttonArray[i, j].Image = new Bitmap(new Bitmap(@"..\..\bomb-icon.png"), gui.buttonArray[i, j].Width - 5, gui.buttonArray[i, j].Height - 5);
+                        }
+
+                    }
+                    if (square.IsFlag)
+                        gui.buttonArray[i, j].Image = new Bitmap(new Bitmap(@"..\..\flag-icon.png"), gui.buttonArray[i, j].Width - 5, gui.buttonArray[i, j].Height - 5);
+                    if (square.isOpen)
+                    {
+                        gui.buttonArray[i, j].Text = square.value.ToString();
+                        gui.buttonArray[i, j].Enabled = false;
+                    }
+                }
         }
 
     }
